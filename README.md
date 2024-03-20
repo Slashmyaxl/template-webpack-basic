df6375b9aba03ba41a1fe3f2376c87a6a655cc45
Slashmyaxl <140274090+Slashmyaxl@users.noreply.github.com>
2024-03-20 13:43:56 -0400

    Initial commit

--- README.md
+++ README.md
@@ -1,1 +1,5 @@
 # template-webpack-basic
+
+This is a basic repo template I've created for all projects which require bundling through webpack. It comes pre-configured with HTML Webpack Plugin, CSS Loaders, and the ability to import common image files. The "inline-source" DevTool is enabled for error tracking, and development mode is initialized.
+
+I've also installed ESLint and Prettier, and ensured that they do not conflict with each other.
---
+++ webpack.config.js
@@ -0,0 +1,29 @@
+const HtmlWebpackPlugin = require("html-webpack-plugin");
+const path = require("path");
+
+module.exports = {
+  mode: "development",
+  entry: "./src/index.js",
+  devtool: "inline-source-map",
+  plugins: [
+    new HtmlWebpackPlugin({
+      title: "Basic Webpack Template w/ HTML plugin",
+    }),
+  ],
+  output: {
+    filename: "main.js",
+    path: path.resolve(__dirname, "dist"),
+  },
+  module: {
+    rules: [
+      {
+        test: /\.css$/i,
+        use: ["style-loader", "css-loader"],
+      },
+      {
+        test: /\.(png|svg|jpg|jpeg|gif)$/i,
+        type: "asset/resource",
+      },
+    ],
+  },
+};
